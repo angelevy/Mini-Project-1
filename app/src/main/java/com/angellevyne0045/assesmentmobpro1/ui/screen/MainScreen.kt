2315d769d051ad77951e6344507089fbd7b7dc0d
Angel Levyne <angellevyne@gmail.com>
2025-03-24 02:28:41 +0700

    Meminta input data dari pengguna

--- app/src/main/java/com/angellevyne0045/assesmentmobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/angellevyne0045/assesmentmobpro1/ui/screen/MainScreen.kt
@@ -1,17 +1,29 @@
 package com.angellevyne0045.assesmentmobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.angellevyne0045.assesmentmobpro1.R
 import com.angellevyne0045.assesmentmobpro1.ui.theme.AssesmentMobpro1Theme
 
@@ -37,10 +49,56 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    var usia by remember { mutableStateOf("") }
+    var berat by remember { mutableStateOf("") }
+    var tinggi by remember { mutableStateOf("") }
+
+    Column (
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ){
+        Text(
+            text = stringResource(id = R.string.bmr_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = usia,
+            onValueChange = { usia = it },
+            label = { Text(text = stringResource(R.string.usia)) },
+            trailingIcon = { Text(text = "Tahun",
+                modifier = Modifier.padding(16.dp))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction =  ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = berat,
+            onValueChange = { berat = it },
+            label = { Text(text = stringResource(R.string.berat_badan)) },
+            trailingIcon = { Text(text = "Kg") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction =  ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = tinggi,
+            onValueChange = { tinggi = it },
+            label = { Text(text = stringResource(R.string.tinggi_badan)) },
+            trailingIcon = { Text(text = "Cm") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction =  ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
